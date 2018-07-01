b6401581c1a276d09470787e1d1091ece5711b64
Manuel Roth <manuelroth@hotmail.ch>
2018-07-01 21:16:41 +0200

    (service-worker) Add build task to generate service-worker for offline capabilities

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp");
+
+gulp.task("generate-service-worker", function(callback) {
+  const path = require("path");
+  const swPrecache = require("sw-precache");
+  const rootDir = "public";
+
+  swPrecache.write(
+    path.join(rootDir, "sw.js"),
+    {
+      staticFileGlobs: [rootDir + "/**/*.{js,html,css,png,jpg,gif}"],
+      stripPrefix: rootDir
+    },
+    callback
+  );
+});
