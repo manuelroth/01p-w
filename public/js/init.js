ae975ef488aa2b8bfc9b77766c2339c467b8edb0
Manuel Roth <manuelroth@hotmail.ch>
2018-07-01 22:53:20 +0200

    (scripts) Add all third-party scripts to js folder so they can be cached by the service-worker

---
+++ public/js/init.js
@@ -0,0 +1,9 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyAnr_FNW6do3QmciHOO3K69EBk1Snf8TZ8",
+  "databaseURL": "https://p-w-7e66e.firebaseio.com",
+  "storageBucket": "p-w-7e66e.appspot.com",
+  "authDomain": "p-w-7e66e.firebaseapp.com",
+  "messagingSenderId": "730322726863",
+  "projectId": "p-w-7e66e"
+});
